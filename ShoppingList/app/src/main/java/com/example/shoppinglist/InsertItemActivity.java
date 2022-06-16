dc461397fcb9e78c9470301920cd218f144e0d49
Szabó Gergő <h051734@stud.u-szeged.hu>
2022-06-16 19:31:32 +0200

    v1.2

--- ShoppingList/app/src/main/java/com/example/shoppinglist/InsertItemActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/InsertItemActivity.java
@@ -2,10 +2,20 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertItemActivity extends AppCompatActivity {
+
+    EditText itemname,quantity;
+    DatabaseReference ref;
+    Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,17 @@ public class InsertItemActivity extends AppCompatActivity {
     }
 
     public void addItem(View view) {
-
+        itemname = (EditText)findViewById(R.id.productnameedit);
+        quantity = (EditText)findViewById(R.id.quantityedit);
+        ref= FirebaseDatabase.getInstance().getReference().child("Item");
+        item = new Item();
+        String name = itemname.getText().toString().trim();
+        int qnty = Integer.parseInt(quantity.getText().toString().trim());
+        item.setName(name);
+        item.setQuantity(qnty);
+        ref.push().setValue(item);
+        Toast.makeText(InsertItemActivity.this,"Item added to shoplist",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
