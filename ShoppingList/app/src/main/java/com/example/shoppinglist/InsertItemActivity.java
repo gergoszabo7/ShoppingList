2e167f647619ee1ec5284d346814083821d1e5bd
Szabó Gergő <h051734@stud.u-szeged.hu>
2022-06-20 13:27:35 +0200

    v1.3

--- ShoppingList/app/src/main/java/com/example/shoppinglist/InsertItemActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/InsertItemActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,25 +17,33 @@ public class InsertItemActivity extends AppCompatActivity {
     EditText itemname,quantity;
     DatabaseReference ref;
     Item item;
+    Button btnadd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_item);
-    }
-
-    public void addItem(View view) {
         itemname = (EditText)findViewById(R.id.productnameedit);
         quantity = (EditText)findViewById(R.id.quantityedit);
+        btnadd = (Button)findViewById(R.id.addbutton);
         ref= FirebaseDatabase.getInstance().getReference().child("Item");
-        item = new Item();
-        String name = itemname.getText().toString().trim();
-        int qnty = Integer.parseInt(quantity.getText().toString().trim());
-        item.setName(name);
-        item.setQuantity(qnty);
-        ref.push().setValue(item);
-        Toast.makeText(InsertItemActivity.this,"Item added to shoplist",Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this,MainActivity.class);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                item = new Item();
+                String name = itemname.getText().toString().trim();
+                int qnty = Integer.parseInt(quantity.getText().toString().trim());
+                item.setName(name);
+                item.setQuantity(qnty);
+                ref.push().setValue(item);
+                Toast.makeText(InsertItemActivity.this,"Item added to shoplist",Toast.LENGTH_LONG).show();
+                switchActivity();
+            }
+        });
+    }
+
+    public void switchActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
